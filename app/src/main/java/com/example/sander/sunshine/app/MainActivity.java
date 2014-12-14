d37bc5dca942823b46161064bff1db8f32d7b983
Sander1992 <sander92@inbox.ru>
2014-12-14 15:54:13 +0200

    Adding networking code. Tryying to use AsynkTask

--- app/src/main/java/com/example/sander/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/sander/sunshine/app/MainActivity.java
@@ -1,19 +1,9 @@
 package com.example.sander.sunshine.app;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -24,7 +14,7 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new ForecastFragment())
                     .commit();
         }
     }
@@ -52,42 +42,4 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            String[] mockData = {
-                    "Monday - 15",
-                    "Tuesday - 20",
-                    "Wednesday - 10",
-                    "Wednesday - 10",
-                    "thurs - 10",
-                    "fri - 10",
-                    "sat - 10",
-                    "sun - 10"
-        };
-
-            List<String> weekForecast = new ArrayList<String>(Arrays.asList(mockData));
-
-            ArrayAdapter<String> forecastAdapter =
-                    new ArrayAdapter<String>(
-                            getActivity(), // The current context (this activity)
-                            R.layout.list_item_forecast, // The name of the layout ID.
-                            R.id.list_item_forecast_textview, // The ID of the textview to populate.
-                            weekForecast);
-
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
-            ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
-            listView.setAdapter(forecastAdapter);
-            return rootView;
-        }
-    }
 }
