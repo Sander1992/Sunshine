4175860fb1d69fbd2945b39f07987c7039a43c80
Sander1992 <sander92@inbox.ru>
2014-12-17 00:17:33 +0200

    Version vith settings activity

--- app/src/main/java/com/example/sander/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/sander/sunshine/app/ForecastFragment.java
@@ -1,5 +1,6 @@
 package com.example.sander.sunshine.app;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -14,7 +15,6 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -99,7 +99,8 @@ public class ForecastFragment extends Fragment {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 String forecast = mForecastAdapter.getItem(position);
-                Toast.makeText(getActivity(), forecast, Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getActivity(), DetailActivity.class).putExtra(Intent.EXTRA_TEXT, forecast);
+                startActivity(intent);
             }
         });
 
